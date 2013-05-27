7f2bf9ae4dee25f254e67e7a802b5b93fb2ec5c9
Stefan Richter <stefan@02strich.de>
2013-05-27 11:26:52 +0200

    better compatibility to normal sqlite3

--- lib/__init__.py
+++ lib/__init__.py
@@ -20,3 +20,5 @@
 # 2. Altered source versions must be plainly marked as such, and must not be
 #    misrepresented as being the original software.
 # 3. This notice may not be removed or altered from any source distribution.
+
+from dbapi2 import *
